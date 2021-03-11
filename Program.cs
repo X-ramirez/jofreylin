4a71b54b7d4ee1b3438e60ff9f00eac4603cabbc
Xiolin Ramirez <66921726+X-ramirez@users.noreply.github.com>
2021-03-10 22:44:25 -0400

    primer commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Probando
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
