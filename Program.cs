388f2491c90dca27874d055e00fee896138cbcda
Jofreylin <trabajosjofrey@hotmail.com>
2021-03-10 22:51:24 -0400

    cambio a Soy Xiolin!

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Probando
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Soy Xiolin!");
         }
     }
 }
